40c77cd61f9e945f71cc7e2420af966e302be04b
Panakova <72735506+Panakova@users.noreply.github.com>
2021-03-16 18:02:14 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -195,7 +195,7 @@ ScreenManager:
         text: "Čo môžeš získať?"
         size_hint: None, None
         text_color: 0,0,0,1
-        pos_hint: {"center_x" :0.25, "center_y":0.0.5}
+        pos_hint: {"center_x" :0.25, "center_y":0.05}
         on_release: root.show_motivation_dialog()    
 
     MDFloatingActionButton:
